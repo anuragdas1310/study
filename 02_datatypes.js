749128d3e6bdffd27b866017e30ca6c5f15b4771
anuragdas1310 <anuragdasad1310@gmail.com>
2025-09-08 12:20:20 +0530

    first commit

---
+++ 02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+// alert("3 + 3")   // we are using nodejs, not browser
+
+console.log(3+
+    3);             // code readability should be high
+console.log("Anurag");
+
+let name = "Anurag"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/fase
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);  // undeffined
+
+console.log(typeof null); // object
+
---
+++ 05_string.js
@@ -0,0 +1,26 @@
+const name = "anurag"
+const repoCount = 50
+// console.log(name + repoCount + " value");
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+const gameName = new String('anurag-hc')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("u"));
+const newString = gameName.substring(0,4)
+console.log(newString);
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+const newStringOne = "   anurag   "
+console.log(newStringOne)
+console.log(newStringOne.trim());
+
+const url = "https://anurag.com/annurag%20das"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('anurag'));
+console.log(gameName.split('-'))
