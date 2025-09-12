5db24bf1d528d74f7ad4c5befcedc40284df6610
anuragdas1310 <anuragdasad1310@gmail.com>
2025-09-12 07:00:50 +0000

    05_objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,30 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "anurag"
+}
+//course.courseInstructor
+
+const {courseInstructor} = course
+
+const {courseInstructor: Instructor} = course
+// console.log(courseInstructor);
+// console.log(Instructor);
+
+// const navbar = ({company}) =>{
+
+// }
+
+// navbar(company = "anurag")
+
+// {
+//     "name": "anurag",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
