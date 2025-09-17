1943908a01d1cb66f58a470bf8454eae84425ed5
anuragdas1310 <anuragdasad1310@gmail.com>
2025-09-17 04:21:51 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {   // if once condition is matched then it will execute all the code except default
+//     case 1:   //in numbers
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("Apr");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const month = "mar"
+switch (month) {
+    case "jan":
+        console.log("Jan")
+        break;
+    case "feb":
+        console.log("Feb")
+        break;
+    case "mar":
+        console.log("Mar")
+        break;
+    case "apr":
+        console.log("Apr")
+        break;
+
+    default:
+        break;
+}
