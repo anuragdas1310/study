eb6c26546b7dbba08c7d2365214bf81d10aa35f1
anuragdas1310 <anuragdasad1310@gmail.com>
2025-09-17 04:05:06 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// const score = 200
+// if (score > 100) {
+//     var power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("balance is less than 500");
+// }
+// else if (balance < 750){
+//     console.log("balance is less than 500");
+
+// }
+// else{
+//     console.log("balance is less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const userLoggedInFromGoogle = false
+const loggedInFromEmail = true
+if (userLoggedIn && debitCard && 2==3){   // in this methd all the conditions are need to be true
+    consol.log("Allow to buy kcourse");
+}
+if (userLoggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in")
+}
